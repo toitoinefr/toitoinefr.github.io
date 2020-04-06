341c79e1649b9f7482d6a355e92cf2dee25ee1bc
Blowz <wayzen.contact@gmail.com>
2020-04-06 04:03:03 +0200

    Design header + scroll top

---
+++ assets/js/utils/time.js
@@ -0,0 +1,19 @@
+export function throttle(callback, delay) {
+    let last
+    let timer
+    return function () {
+        const context = this
+        const now = +new Date()
+        const args = arguments
+        if (last && now < last + delay) {
+            clearTimeout(timer)
+            timer = setTimeout(function () {
+                last = now
+                callback.apply(context, args)
+            }, delay)
+        } else {
+            last = now
+            callback.apply(context, args)
+        }
+    }
+}
